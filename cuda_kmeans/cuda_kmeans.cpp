4095b28968271db0a64d89f74f0d33b5e0f0c70a
kaunild <dhruv.kaunil@gmail.com>
2020-01-07 08:31:56 -0700

    Add project files.

---
+++ cuda_kmeans/cuda_kmeans.cpp
@@ -0,0 +1,12 @@
+﻿// cuda_kmeans.cpp : Defines the entry point for the application.
+//
+
+#include "cuda_kmeans.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
